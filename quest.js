8ad1ac05acd2d2d62969528268077620ecd855b7
ruggz13 <nicolasruggieri7@gmail.com>
2019-12-03 14:03:04 -0500

    first commit

--- quest.js
+++ quest.js
@@ -1,36 +1,62 @@
-console.log('quest.js loaded')
+console.log("quest.js loaded");
 
-quest=function(){
-    //ini
-}
+quest = function() {
+  //ini
+};
 
-quest.render=(txt)=>{
-    var html=''
-    var txt0=txt
+quest.render = txt => {
+  var html = "";
+  var txt0 = txt;
 
-    // remove blocked out
-    txt=txt.replace(/\/\*[\s\S]+\*\//g,'')
-    txt=txt.replace(/\/\/.*\n/g,'')
+  // remove blocked out
+  txt = txt.replace(/\/\*[\s\S]+\*\//g, "");
+  txt = txt.replace(/\/\/.*\n/g, "");
 
-    // separate  questions
-    txt = txt.split(/\n\n/).map(qq=>{
-        qq=qq.split('\n')
-        if(qq.length==1){
-            //html += `<h3>${qq[0]} <span style="font-size:small;cursor:hand;color:blue">[hide]</span></h3>`
-            html += `<h3>${qq[0]}</h3>`
-        }else if(qq.length>1){
-            html += `<p><b>${qq[0]}</b><br>`
-            qq.slice(1).forEach(q=>{
-                 html += `${q}<br>`
-            })
-            html += '</p>'
-        }
-    })
+  // separate  questions
+  txt = txt.split(/\n\n/).map(qq => {
+    qq = qq.split("\n");
+    if (qq.length == 1) {
+      //html += `<h3>${qq[0]} <span style="font-size:small;cursor:hand;color:blue">[hide]</span></h3>`
+      html += `<h3>${qq[0]}</h3>`;
+    } else if (qq.length > 1) {
+      html += `<p><b>${qq[0]}</b><br>`;
+      qq.slice(1).forEach(q => {
+        html += `${q}<br>`;
+      });
+      html += "</p>";
+    }
+  });
 
-    // html elements
-    html = html.replace(/\[\]/g,'<input type="checkbox">')
-    html = html.replace(/\|__\|/g,'<input>')
-    html = html.replace(/\|___\|/g,'<textarea></textarea>')
+  // ---- html elements ---- //
 
-    return html+'<hr>'//+txt0
-}
+  // Check Box []
+  html = html.replace(/\[\]/g, '<input type="checkbox">');
+  html = html.replace(/[\n\r]/g, '<input type="checkbox">');
+
+  // Integer |_|
+  html = html.replace(/\|_\|/g, "<input type='number'>");
+
+  // Regular input field |__|
+  html = html.replace(/\|__\|/g, "<input>");
+
+  // Text Area |___|
+  html = html.replace(/\|___\|/g, "<textarea></textarea>");
+
+  html = html.replace(/\[text box\]/g, "<textarea></textarea>");
+
+  // Phone Number |(###)-###-####|
+  html = html.replace(
+    /\|\(\###\)\-###-####\|/g,
+    "<input type='tel' id='phone' name='phone' pattern='(((d{3}) ?)|(d{3}-))?d{3}-d{4}'>"
+  );
+
+  // Social Security |###-##-####|
+  html = html.replace(
+    /\|###-##-####\|/g,
+    "<input type='tel' id='social' name='social' pattern='^(?!219099999|078051120)(?!666|000|9d{2})d{3}(?!00)d{2}(?!0{4})d{4}$'>"
+  );
+
+  return html + "<hr>"; //+txt0
+};
+
+class Questionnaire {}
