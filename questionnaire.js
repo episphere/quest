367cc2e0b2e25d603763d8400ecf7ece16c71001
Daniel Russ <druss@nih.gov>
2020-01-13 11:42:25 -0500

    added tree structure for branching questionnaires

--- questionnaire.js
+++ questionnaire.js
@@ -15,47 +15,47 @@ function clearSelection(inputElement) {
     }
 }
 
+
+// The questionQueue is an array which contains
+// the question we should go to next.
+const questionQueue = []
+
 // norp == next or previous button (which ever is clicked...)
 function next(norp) {
 
-    // the default next question is ... well next...
-    var nextElement = norp.parentElement.nextElementSibling
+    // check if we need to add questions to the question queue
+    checkForSkips(norp.parentElement);
+
+    // get the next question from the questionQueue
+    // if it exists... otherwise get the next Element
+    var nextElement = null;
+    if (questionQueue.length > 0){
+        nextElement = questionQueue.shift()
+    } else {
+        nextElement = norp.parentElement.nextElementSibling
+    }
+
+
+    // hide the current question and move to the next...
     norp.parentElement.classList.remove("active");
-
-    // does the selected response element have a skip???
-    var skipTo = checkForSkips(norp.parentElement);
-    if (skipTo != null) {
-        nextElement = document.getElementById(skipTo);
-    }
-
-    // should we really display the next element???
-    if (nextElement.hasAttribute("showIfId")) {
-        var acceptableValues = nextElement.getAttribute("values").split(",");
-        var checkValueFromElement = document.getElementById(nextElement.getAttribute("showIfId"));
-        var selectedValues = getSelected(checkValueFromElement);
-
-        var intersection = selectedValues.filter(x => acceptableValues.includes(x))
-        if (intersection.length == 0) {
-            console.log("[" + acceptableValues + "] does not contain any of the selectedValues: " + selectedValues + " so I am skipping " + nextElement.id + " and going to " + nextElement.nextElementSibling.id);
-            nextButton = nextElement.querySelector(".next");
-            if (nextButton != null) {
-                nextElement = next(nextButton);
-                return (nextElement);
-            } else {
-                nextElement = nextElement.nextElementSibling;
-            }
-
-        }
-    }
-
     nextElement.classList.add("active");
+
+    // by the way...  set the next question's previous button's skipTo element...
+    var prevButtonList = nextElement.getElementsByClassName("previous");
+    if (prevButtonList.length>0) {
+        prevButtonList[0].skipTo=norp.parentElement.id;
+    }
+
     return (nextElement)
 }
 
 function prev(norp) {
-    var prevElement = norp.parentElement.previousElementSibling
-    norp.parentElement.classList.remove("active")
-    prevElement.classList.add("active")
+    if (norp.hasAttribute("skipTo")){
+        var prevElement = document.getElementById(norp.skipTo);
+        norp.parentElement.classList.remove("active")
+        prevElement.classList.add("active")
+    }
+
 
     var skipTo = checkForSkips(norp.parentElement);
     if (skipTo != null) {
@@ -64,46 +64,108 @@ function prev(norp) {
     return (prevElement);
 }
 
+// this function just adds questions to the
+// question queue.  It always returns null;
 function checkForSkips(questionElement) {
-    selectedElements = getSelected(questionElement, true);
-    // if more than 1 element is selected...
-    // cannot be a skip....
-    if (selectedElements.length == 1) {
-        if (selectedElements[0].hasAttribute("skipTo")) {
-            return selectedElements[0].getAttribute("skipTo");
-        }
-    }
+    // get selected responses
+    selectedElements = getSelected(questionElement);
+
+    // if there is a skipTo attribute, add them to the beginning of the queue...
+    // add the selected responses to the question queue
+    selectedElements = selectedElements.filter( x => x.hasAttribute("skipTo")); 
+    
+    // add all the selected elements with the skipTo attribute to the question queue
+    questionQueue.unshift(...selectedElements)
+
     return (null);
 }
 
-function getSelected(questionElement, returnElement = false) {
-    // check for checkboxes...
-    var cb = questionElement.querySelectorAll("input[type='checkbox']");
-    if (cb.length > 0) {
-        return (getChecked(cb, returnElement))
-    }
-    // check for radiogroup
-    cb = questionElement.querySelectorAll("input[type='radio']");
-    if (cb.length > 0) {
-        return (getChecked(cb, returnElement))
-    }
+function getSelected(questionElement) {
 
-    var rv = [];
+    // look for radio boxes, checkboxes, and  hidden elements
+    // for checked items.  Return all checked items.
+    // If nothing is checked, an empty array should be returned.
+    var cb = [...questionElement.querySelectorAll("input[type='radio'],input[type='checkbox'],input[type='hidden'")];
+    cb = cb.filter( x => x.checked)
     return (rv)
 }
 
-function getChecked(elementArray, returnElement) {
-    var values = [];
-    for (var x of elementArray) {
-        if (x.checked) {
-            if (returnElement) {
-                values.push(x)
-            } else {
-                values.push(x.value)
-            }
-        }
+class Tree{
+    constructor(root){
+        this.current = root;
+
+        this[Symbol.iterator] = function() { 
+            return this;
+        };
     }
-    return (values)
+
+
+    next(){
+        let tmp = this.current.next();
+        if (!tmp.done){
+            let rv = {done: false, value:this.current}
+            this.current = tmp.value;
+            return(rv);
+        }
+
+        return(tmp);
+    }
+}
+
+class TreeNode {
+
+    constructor(value){
+        this.value = value;
+        this.parent = null;
+        this.children = [];
+
+    }
+
+    setParent(parent){
+        this.parent=parent;
+    }
+
+    addChild(child){
+        child.parent=this;
+        this.children.push(child);
+    }
+
+    lookForNext(child){
+        // child asked for the next node ...
+        // lets find his index....
+        let childIndex = this.children.indexOf(child)
+        // not sure how the index could not be found...
+        // unless misused...
+        if (childIndex == -1) {
+            return undefined;
+        }
+
+        // get the next index and if
+        // it is still a valid index
+        if ( ++childIndex < this.children.length){
+            //return this.children[childIndex];
+            return {done: false, value: this.children[childIndex]}
+        }
+        // child was the last element of the array,
+        // so ask our parent for the next element...
+        // but if we are the root..  return null...
+        if (this.parent == null){
+            return {done: true, value: undefined};
+        }
+        return this.parent.lookForNext(this);
+    }
+
+    next(){
+        if (this.children.length>0){
+            return {done: false, value: this.children[0]};
+        }
+        if (this.parent == null) return {done: true}; 
+        return this.parent.lookForNext(this);
+    }
+    
+   iterator(){
+       return new Tree(this);
+   }
 }
 
 
