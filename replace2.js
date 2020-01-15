b36de7b4d8a55c75cb64170385a1a7694c9463b0
Nick Ruggieri <nicolasruggieri7@gmail.com>
2020-01-15 09:16:51 -0500

    added changes to index.html

--- replace2.js
+++ replace2.js
@@ -128,6 +128,36 @@ transform.render = contents => {
       // replace user profile variables...
       z = z.replace(/{\$u:(\w+)}/, "<span name='$1'>$1</span>");
 
+      // //create modal
+      // z = z.replace(
+      //   /{(\w+)}/gms,
+      //   ` <!-- Button trigger modal -->
+      // <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      //     $1
+      // </button>
+
+      //   <!-- Modal -->
+      //   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      //     <div class="modal-dialog" role="document">
+      //       <div class="modal-content">
+      //         <div class="modal-header">
+      //           <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+      //           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      //             <span aria-hidden="true">&times;</span>
+      //           </button>
+      //         </div>
+      //         <div class="modal-body">
+      //           ...
+      //         </div>
+      //         <div class="modal-footer">
+      //           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      //           <button type="button" class="btn btn-primary">Save changes</button>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>`
+      // );
+
       // handle skips
       z = z.replace(
         /<input (.*?)><\/input><label(.*?)>(.*?)\s*->\s*(.*?)<\/label>/g,
