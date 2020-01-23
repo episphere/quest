9457bd013c1d5751561e8f68553ac4d8394ce46c
Nick Ruggieri <nicolasruggieri7@gmail.com>
2020-01-23 16:28:22 -0500

    added SSN, phone number, dates, and state dropdown

--- replace2.js
+++ replace2.js
@@ -1,14 +1,3 @@
-// var fs = require('fs');
-// var lineReader = require('line-reader');
-
-var z = "";
-var qid = "";
-
-// fs.readFile('textfile.z', 'utf8',
-//     function(err, contents) {
-//         transform(contents);
-//     });
-
 transform = function() {
   // ini
 };
@@ -73,6 +62,79 @@ transform.render = contents => {
     if (hardBool || softBool) {
       y = y.slice(0, -1);
     }
+
+    // replace __/__/__ with a date input
+    z = z.replace(/\_\_\/\_\_\/\_\_/g, "<input type='date'></input>");
+
+    // replace (###)-###-#### with phone input
+    z = z.replace(
+      /\(###\)-###-####/g,
+      "<input type='tel' name='phone' pattern='(([0-9]{3})|[0-9]{3})-[0-9]{3}-[0-9]{4}' required></input>"
+    );
+
+    // replace (###)-###-#### with SSN input
+    z = z.replace(
+      /\|###-##-####\|/g,
+      "<input pattern='[0-9]{3}-[0-9]{2}-[0-9]{4}' required></input>"
+    );
+
+    // replace |state| with state dropdown
+    z = z.replace(
+      /\|state\|/g,
+      `<select id='state'>
+      <option value='AL'>Alabama</option>
+      <option value='AK'>Alaska</option>
+      <option value='AZ'>Arizona</option>
+      <option value='AR'>Arkansas</option>
+      <option value='CA'>California</option>
+      <option value='CO'>Colorado</option>
+      <option value='CT'>Connecticut</option>
+      <option value='DE'>Delaware</option>
+      <option value='DC'>District Of Columbia</option>
+      <option value='FL'>Florida</option>
+      <option value='GA'>Georgia</option>
+      <option value='HI'>Hawaii</option>
+      <option value='ID'>Idaho</option>
+      <option value='IL'>Illinois</option>
+      <option value='IN'>Indiana</option>
+      <option value='IA'>Iowa</option>
+      <option value='KS'>Kansas</option>
+      <option value='KY'>Kentucky</option>
+      <option value='LA'>Louisiana</option>
+      <option value='ME'>Maine</option>
+      <option value='MD'>Maryland</option>
+      <option value='MA'>Massachusetts</option>
+      <option value='MI'>Michigan</option>
+      <option value='MN'>Minnesota</option>
+      <option value='MS'>Mississippi</option>
+      <option value='MO'>Missouri</option>
+      <option value='MT'>Montana</option>
+      <option value='NE'>Nebraska</option>
+      <option value='NV'>Nevada</option>
+      <option value='NH'>New Hampshire</option>
+      <option value='NJ'>New Jersey</option>
+      <option value='NM'>New Mexico</option>
+      <option value='NY'>New York</option>
+      <option value='NC'>North Carolina</option>
+      <option value='ND'>North Dakota</option>
+      <option value='OH'>Ohio</option>
+      <option value='OK'>Oklahoma</option>
+      <option value='OR'>Oregon</option>
+      <option value='PA'>Pennsylvania</option>
+      <option value='RI'>Rhode Island</option>
+      <option value='SC'>South Carolina</option>
+      <option value='SD'>South Dakota</option>
+      <option value='TN'>Tennessee</option>
+      <option value='TX'>Texas</option>
+      <option value='UT'>Utah</option>
+      <option value='VT'>Vermont</option>
+      <option value='VA'>Virginia</option>
+      <option value='WA'>Washington</option>
+      <option value='WV'>West Virginia</option>
+      <option value='WI'>Wisconsin</option>
+      <option value='WY'>Wyoming</option>
+    </select>`
+    );
 
     // replace |__|__|__|__|  with a number box...
     z = z
