4c62ac4422af84461733bca581d919805f6d76d6
bhaumik55231 <bhaumik55231@gmail.com>
2020-07-06 17:20:55 -0400

    Bug fix scrollintoview

--- questionnaire.js
+++ questionnaire.js
@@ -340,8 +340,8 @@ async function nextPage(norp, store) {
   //hide the current question
   questionElement.classList.remove("active");
   // nextElement.scrollIntoView();
-  nextElement.querySelectorAll(".questionLabel")[0].scrollIntoView();
   displayQuestion(nextElement);
+  nextElement.querySelectorAll(".questionLabel")[0].scrollIntoView();
 }
 
 export function displayQuestion(nextElement) {
